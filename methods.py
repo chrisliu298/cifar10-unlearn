ec78930a5b40a3c14de1ffc8cb4d3b5ec53b1d34
chrisliu298 <59010212+chrisliu298@users.noreply.github.com>
2023-10-25 09:56:14 -0700

    Fix ga

--- methods.py
+++ methods.py
@@ -36,8 +36,20 @@ def finetune(net, optimizer, criterion, scheduler, loader, device):
 
 
 def gradient_ascent(net, optimizer, criterion, scheduler, loader, device):
-    neg_criterion = lambda logits, y: -criterion(logits, y)
-    train(net, optimizer, neg_criterion, scheduler, loader, device)
+    net.train()
+    loss, acc = 0, 0
+    for x, y in loader:
+        x, y = x.to(device), y.to(device)
+        logits = net(x)
+        loss = -criterion(logits, y)
+        optimize(optimizer, loss)
+        loss += loss.item()
+        acc += (logits.argmax(dim=-1) == y).float().mean().item()
+        wandb.log({"grad_norm": grad_norm(net), "weight_norm": weight_norm(net)})
+    loss /= len(loader)
+    acc /= len(loader)
+    scheduler.step()
+    return loss, acc
 
 
 def rand_labels(net, optimizer, criterion, scheduler, loader, device):
